954dd7d463d53ae7625c23a73170163402fc2afc
me-mystic <Shrutikolla.2002@gmail.com>
2023-05-18 21:08:12 +0530

    .

---
+++ harry/htmls/js/23-Mathobj.js
@@ -0,0 +1,24 @@
+// math obj
+const arr = [4,6];
+z = Math;
+z = Math.E;// euler's val
+z = Math.PI;
+z = Math.round(5.3);
+z = Math.ceil(5.1);
+z = Math.floor(-5.3);
+z = Math.sqrt(4.3);
+z = Math.pow(2.5, 2);
+z = Math.abs(-3.5)
+z = Math.min(-3.5, 34,23,-21,43);
+z = Math.max(-3.5, 34,23,-21,43);
+
+// Math.random
+
+z = 100*Math.random();
+z = 90 + 10*Math.random(); // --> 90 to 100
+/*
+a = (0,1)
+a100 = a*100 = (0,100)
+a10_100 = 10 + (100-10) * a = (10, 100)
+*/ 
+console.log(z);
