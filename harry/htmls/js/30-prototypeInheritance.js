66494ed9763a31a34beb8f7cefa6dbefae0f3011
me-mystic <Shrutikolla.2002@gmail.com>
2023-05-24 17:15:50 +0530

    .

--- harry/htmls/js/30-prototypeInheritance.js
+++ harry/htmls/js/30-prototypeInheritance.js
@@ -25,3 +25,29 @@ let HarryObj = Object.create(proto, {
 HarryObj.changeName('jbwe');
 HarryObj.slogan()
 console.log(HarryObj)   
+
+// Employee constructor
+
+const Employee = function(name, experience, salary) {
+    this.name = name;
+    this.experience =experience;
+    this.salary = salary;
+}
+
+Employee.prototype.slogan = function() {
+    console.log("slogan");
+}
+let e1 = new Employee('hgv', 0,2);
+console.log(e1);
+
+// Programmer constructor --> inheriting from employee
+
+const Programmer = function(name, experience, salary, language) {
+    Employee.call(this, name, experience, salary);
+    this.language = language;
+}
+Programmer.prototype = Object.create(Employee.prototype);
+Programmer.prototype.constructor = Programmer;
+let p1 = new Programmer('hf', 2, 0, 'cds');
+p1.slogan();
+console.log(p1);
