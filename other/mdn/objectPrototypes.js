df296146d3ecaad71c211c09718f872e3a1495ee
me-mystic <Shrutikolla.2002@gmail.com>
2023-05-26 09:34:58 +0530

    .

--- other/mdn/objectPrototypes.js
+++ other/mdn/objectPrototypes.js
@@ -28,3 +28,45 @@ do {
     object = Object.getPrototypeOf(object);
     console.log(object)
 }while(object);
+
+/* 
+setting prototype
+    1. Object.create
+*/ 
+
+const PersonPrototype = {
+    bio() {
+        console.log(`I'm ${this.name}`);
+    }
+}
+
+// creating a person boj with PersonPrototype as Prototype
+const p1 = Object.create(PersonPrototype);
+console.log((p1))
+p1.name = 'shruti'
+console.log(p1.name)
+console.log(p1.bio())
+
+/*
+    2. Object.assign --> using constructor
+*/ 
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+Object.assign(Person.prototype, PersonPrototype);
+// Person.prototype = PersonPrototype;
+const p2 = new Person('vysh', 18);
+console.log(p2);
+console.log(Object.getPrototypeOf(p2));
+
+/*
+Own Property
+    1. hasOwn(ObjName, prptyName)
+    2. hasOwnProperty()
+*/ 
+console.log(p2.hasOwnProperty('name')); //true
+console.log(p2.hasOwnProperty('bio')); //false          
+console.log(Object.hasOwn(Person,  'name')); //true
+console.log(Object.hasOwn(Person,  'bio')); //false
