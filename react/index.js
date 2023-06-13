7db39af4a350e9a63ce921c9d3062d2bff6382c4
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-13 23:06:16 +0530

    .

---
+++ react/index.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(<h1>this is shruu</h1>, document.getElementById('root'))
+ReactDOM.render(
+<p>A day in my life
+<ul>
+    <li>snack</li>
+    <li>study</li>
+    <li>binge</li>
+    <li>sleep</li>
+</ul></p>, document.getElementById('root'))
