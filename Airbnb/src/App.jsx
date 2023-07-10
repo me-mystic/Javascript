41faa562a83866367ad13b4584bac082d7bf5d74
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-10 10:24:55 +0530

    .

---
+++ Airbnb/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//components
+import Nav from '../src/Components/Navbar.jsx';
+// import './App.css'
+
+function App() {
+  return (
+    <>
+        <Nav/>
+    </>
+  )
+}
+
+export default App
